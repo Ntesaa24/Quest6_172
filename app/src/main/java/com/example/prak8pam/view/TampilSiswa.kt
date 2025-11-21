4bf1238e2373496e20946a7652edd120d909ec06
Ntesaa24 <notesa.aldinasari24@gmail.com>
2025-11-21 14:23:49 +0700

    perbaikan 1 TampilSiswa.kt

--- app/src/main/java/com/example/prak8pam/view/TampilSiswa.kt
+++ app/src/main/java/com/example/prak8pam/view/TampilSiswa.kt
@@ -23,52 +23,15 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.prak8pam.R
+import com.example.prak8pam.model.Siswa
+
+private val Siswa.alamat: Any
+private val Siswa.gender: Any
+private val Siswa.nama: Any
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
-	onBackBtnClick: ()->Unit
-){
-	val items = listOf(
-		Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
-		Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
-		Pair(stringResource(id = R.string.alamat), "Yogyakarta")
-	)
-	Scaffold (modifier = Modifier,
-		{
-			TopAppBar(
-				title = {Text(text = stringResource(id=R.string.tampil), color = Color.White)},
-				colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id=R.color.teal_700))
-			)
-		}) { isiRuang ->
-		Column(
-			modifier = Modifier.padding(isiRuang),
-			verticalArrangement = Arrangement.SpaceBetween
-		) {
-			Column(
-				modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
-				verticalArrangement = Arrangement.spacedBy(dimensionResource(id =  R.dimen.padding_small))
-			) {
-				items.forEach { item ->
-					Column {
-						Text(text = item.first.uppercase(), fontSize = 16.sp)
-						Text(
-							text = item.second,
-							fontWeight = FontWeight.Bold,
-							fontFamily = FontFamily.Cursive,
-							fontSize = 22.sp
-						)
-					}
-					HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
-				}
-				Spacer(modifier = Modifier.height(10.dp))
-				Button(
-					modifier = Modifier.fillMaxWidth(),
-					onClick = onBackBtnClick
-				) {
-					Text(text = stringResource(id = R.string.back))
-				}
-			}
-		}
-	}
-}
+	statusUiSiswa: Siswa,
+	onBackBtnClicked: ()->Unit
+	){}

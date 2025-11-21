351c5477413ed52fb49984f382ea86d4449160ae
Ntesaa24 <notesa.aldinasari24@gmail.com>
2025-11-21 14:56:11 +0700

    membuat formisian

--- app/src/main/java/com/example/prak8pam/view/Formulir.kt
+++ app/src/main/java/com/example/prak8pam/view/Formulir.kt
@@ -7,8 +7,10 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -20,69 +22,8 @@ import androidx.compose.ui.graphics.Color
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(
-	jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
-	OnSubmitBtnClick : () -> Unit
-){
-	Scaffold (modifier=Modifier,
-		topBar = {
-			TopAppBar(
-				title = {Text(text=stringResource(id=R.string.home), color =  Color.White)},
-				colors = TopAppBarDefaults.mediumTopAppBarColors(
-					containerColor = colorResource(id = R.color.teal_700))
-			)}
-	){ isiRuang ->
-		Column(modifier = Modifier.padding(paddingValues = isiRuang),
-			verticalArrangement = Arrangement.Top,
-			horizontalAlignment = Alignment.CenterHorizontally){
-			OutlinedTextField(
-				value = "",
-				singleLine = true,
-				modifier = Modifier
-					.padding(top = 20.dp)
-					.width(width = 250.dp),
-				label = {Text(text = "Nama Lengkap")},
-				onValueChange = {},
-			)
-			HorizontalDivider(modifier = Modifier
-				.padding(all=20.dp)
-				.width(width=250.dp), thickness = Thickness, color=Color.Red)
-			Row{
-				jenisK.forEach {
-						item->
-					Row(verticalAlignment = Alignment.CenterVertically) {
-						RadioButton(
-							selected = false,
-							onClick = { item }
-						)
-						Text(text = item)
-					}
-				}
-			}
+	pilihanJK: List<String>,
+	OnSubmitButtonClicked : (MutableList<String>) -> Unit,
+	modifier: Modifier = Modifier
 
-			HorizontalDivider(modifier = Modifier
-				.padding(all=20.dp)
-				.width(width=250.dp),
-				thickness = 1.dp,
-				color=Color.Red
-			)
-			OutlinedTextField(
-				value = "",
-				singleLine = true,
-				modifier = Modifier
-					.width(width = 250.dp),
-				label = {Text(text = "Alamat")},
-				onValueChange = {},
-			)
-			Spacer(modifier = Modifier.height(height = 30.dp))
-			Button(
-				modifier = Modifier.fillMaxWidth(1f)
-					.padding(all = 25.dp),
-				onClick = OnSubmitBtnClick
-			){
-				Text(text = stringResource(id = R.string.submit))
-
-			}
-
-		}
-	}
-}
+)
